14cc6b19c583f15de2aebfd1bfcea590394e6950
OmarTriguiwx <triguiomar95@gmail.com>
2017-03-14 14:53:27 +0100

    first workdone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,22 +77,8 @@
                 </div>
             </div>
         </nav>
-            @if(session('success'))
-                <div class="container">
-                    <div class="alert alert-success">
-                        {{session('success')}}
-                    </div> 
-                </div>
-            @endif
-             @if(session('error'))
-                <div class="container">
-                    <div class="alert alert-danger">
-                        {{session('error')}}
-                    </div> 
-                </div>
-            @endif
-        @yield('content')
 
+        @yield('content')
     </div>
 
     <!-- Scripts -->
